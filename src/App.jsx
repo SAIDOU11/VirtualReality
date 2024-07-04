3fc48ed8f4adefbd62d811e0a3ea23f3f52caf7d
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-07-04 18:11:20 +0200

    Virtual Reality
    Virtual Reality project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-500 underline m-4">
+        Hello world!
+      </h1>
+      <h2 className="text-2xl text-center font-poppins">Font Poppins</h2>
+    </div>
+  );
+};
+
+export default App;
