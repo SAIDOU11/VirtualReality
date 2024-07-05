c1835a441098eb2fac6a27374642c6db4e77c327
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-07-05 17:55:40 +0200

    text-sm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a href="#" className="py-2 px-3 border rounded-md">
+            <a href="#" className="py-2 px-3 border rounded-md text-sm">
               Sign In
             </a>
             <a
