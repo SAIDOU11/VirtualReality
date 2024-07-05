632849847bd4092551ba1f6d2da6b09a1427a453
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-07-05 10:57:51 +0200

    setup logo, navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <Navbar />
       <h1 className="text-3xl font-bold text-red-500 underline m-4">
         Hello world!
       </h1>
       <h2 className="text-2xl text-center font-poppins">Font Poppins</h2>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl text-blue-300">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
